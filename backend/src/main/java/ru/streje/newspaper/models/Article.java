3e409f1d41049ae268b06da34faa6dbc09d1816f
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-17 21:35:22 +0300

    Добавлен класс-модель для Article

---
+++ backend/src/main/java/ru/streje/newspaper/models/Article.java
@@ -0,0 +1,46 @@
+package ru.streje.newspaper.models;
+
+import java.sql.Date;
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "article")
+@Data
+public class Article {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "text")
+	private String text;
+
+	@Column(name = "image")
+	private byte[] image;
+
+	@Column(name = "date")
+	private Date date;
+
+	@ManyToMany
+	@JoinTable(name = "themes_articles", joinColumns = @JoinColumn(name = "article_id"), inverseJoinColumns = @JoinColumn(name = "theme_id"))
+	private Collection<Theme> themes;
+
+	@ManyToMany
+	@JoinTable(name = "likes", joinColumns = @JoinColumn(name = "article_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private Collection<User> users;
+
+}
