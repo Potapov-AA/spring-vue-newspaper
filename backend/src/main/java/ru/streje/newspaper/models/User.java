840449cb3478cbcfabda4cc6f95330bec127d703
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-17 14:51:48 +0300

    Прописана модель пользователей

--- backend/src/main/java/ru/streje/newspaper/models/User.java
+++ backend/src/main/java/ru/streje/newspaper/models/User.java
@@ -1,5 +1,42 @@
 package ru.streje.newspaper.models;
 
-public class User {
+import java.util.Collection;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "firstname")
+	private String firstname;
+	
+	@Column(name = "lastname")
+	private String lastname;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "email")
+	private String email;
+
+	@ManyToMany
+	@JoinTable(name = "users_roles", 
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Collection<Role> roles;
 }
