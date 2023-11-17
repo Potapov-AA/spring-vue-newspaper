f9b6f6a66622f54e14ffd8ded1be091290a79398
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-17 16:48:39 +0300

    Переименован класс

--- backend/src/main/java/ru/streje/newspaper/messages/ErrorMessage.java
+++ backend/src/main/java/ru/streje/newspaper/messages/ErrorMessage.java
@@ -1,16 +1,16 @@
-package ru.streje.newspaper.exeptions;
+package ru.streje.newspaper.messages;
 
 import java.util.Date;
 
 import lombok.Data;
 
 @Data
-public class AppError {
+public class ErrorMessage {
 	private int status;
 	private String message;
 	private Date timestamp;
 
-	public AppError(int status, String message) {
+	public ErrorMessage(int status, String message) {
 		this.status = status;
 		this.message = message;
 		this.timestamp = new Date();
