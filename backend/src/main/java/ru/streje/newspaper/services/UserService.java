cff177a69c1573f4021132e893f19338731f3bd1
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-17 15:27:23 +0300

    Сервис бизнес логики для работы с User

--- backend/src/main/java/ru/streje/newspaper/services/UserService.java
+++ backend/src/main/java/ru/streje/newspaper/services/UserService.java
@@ -1,5 +1,63 @@
 package ru.streje.newspaper.services;
 
-public class UserService {
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import ru.streje.newspaper.dtos.RegistrationUserRequest;
+import ru.streje.newspaper.models.User;
+import ru.streje.newspaper.repositories.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+	private final UserRepository userRepository;
+	private final RoleService roleService;
+	private final PasswordEncoder passwordEncoder;
+	
+	public Optional<User> findByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+	
+	@Transactional
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		User user = findByEmail(email).orElseThrow(
+				() -> new UsernameNotFoundException(String.format("Пользователь с данной почтой '%s' не найден", email)));
+		
+		return new org.springframework.security.core.userdetails.User(
+				user.getEmail(),
+				user.getPassword(),
+				user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList()));
+	}
+	
+	public void createNewUser(RegistrationUserRequest registrationUserRequest) {
+		User user = new User();
+		user.setEmail(registrationUserRequest.getEmail());
+		user.setFirstname(registrationUserRequest.getFirstname());
+		user.setLastname(registrationUserRequest.getLastname());
+		user.setPassword(passwordEncoder.encode(registrationUserRequest.getPassword()));
+		user.setRoles(List.of(roleService.getUserRole()));
+		userRepository.save(user);
+	}
+	
+	//ПО ИДЕЕ ПОКА ЧТО НЕ НУЖНЫ
+	public Integer getUserId(String email) {
+		User user = findByEmail(email).get();
+		return user.getId();
+	}
+	
+	public User getUser(String email) {
+		return findByEmail(email).get();
+	}
 }
