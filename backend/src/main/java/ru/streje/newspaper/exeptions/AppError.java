8192cddd9e4d2e89ab393e39888f320d61c4140e
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-17 15:09:51 +0300

    Прописан класс-ошибка для возвращения информации об ошибках

--- backend/src/main/java/ru/streje/newspaper/exeptions/AppError.java
+++ backend/src/main/java/ru/streje/newspaper/exeptions/AppError.java
@@ -1,5 +1,18 @@
 package ru.streje.newspaper.exeptions;
 
-public class AppError {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class AppError {
+	private int status;
+	private String message;
+	private Date timestamp;
+
+	public AppError(int status, String message) {
+		this.status = status;
+		this.message = message;
+		this.timestamp = new Date();
+	}
 }
