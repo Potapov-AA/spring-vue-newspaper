e2217402955143b8a069915ee5abb65defbc1436
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-17 15:42:57 +0300

    Сервис бизнес логики авторизации

--- backend/src/main/java/ru/streje/newspaper/services/AuthService.java
+++ backend/src/main/java/ru/streje/newspaper/services/AuthService.java
@@ -1,5 +1,56 @@
 package ru.streje.newspaper.services;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import lombok.RequiredArgsConstructor;
+import ru.streje.newspaper.dtos.JwtRequest;
+import ru.streje.newspaper.dtos.JwtResponse;
+import ru.streje.newspaper.dtos.RegistrationUserRequest;
+import ru.streje.newspaper.exeptions.AppError;
+import ru.streje.newspaper.utilis.JwtTokenUtils;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
 
+	private final UserService userService;
+	private final JwtTokenUtils jwtTokenUtils;
+	private final AuthenticationManager authenticationManager;
+
+	public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+			return new ResponseEntity<>(new AppError(HttpStatus.UNAUTHORIZED.value(), "Неправильный логин или пароль"),
+					HttpStatus.UNAUTHORIZED);
+		}
+
+		UserDetails userDetails = userService.loadUserByUsername(authRequest.getEmail());
+		String token = jwtTokenUtils.generateToken(userDetails);
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+
+	public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserRequest registrationUserRequest) {
+		if (!registrationUserRequest.getPassword().equals(registrationUserRequest.getConfirmPassword())) {
+			return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "Пароли не совпадают"),
+					HttpStatus.BAD_REQUEST);
+		}
+
+		if (userService.findByEmail(registrationUserRequest.getEmail()).isPresent()) {
+			return new ResponseEntity<>(
+					new AppError(HttpStatus.BAD_REQUEST.value(), "Пользователь с данной почтой уже существует"),
+					HttpStatus.BAD_REQUEST);
+		}
+
+		userService.createNewUser(registrationUserRequest);
+		return ResponseEntity.ok(HttpStatus.OK);
+	}
 }
