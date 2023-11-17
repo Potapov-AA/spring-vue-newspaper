b13a0c214cece62335955b2701f552cab2553c48
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-17 21:35:43 +0300

    Добавлен класс-модель для Comment

---
+++ backend/src/main/java/ru/streje/newspaper/models/Comment.java
@@ -0,0 +1,32 @@
+package ru.streje.newspaper.models;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "text")
+	private String text;
+	
+	@Column(name = "date")
+	private Date date;
+	
+	@ManyToOne
+	private Article article;
+	
+	@ManyToOne
+	private User user;
+}
