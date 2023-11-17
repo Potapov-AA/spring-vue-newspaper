ffd56d1ac6f93cac5d88b4f98d91bcc81beb4408
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-17 14:54:24 +0300

    Прописан репозиторий для ролей

--- backend/src/main/java/ru/streje/newspaper/repositories/RoleRepository.java
+++ backend/src/main/java/ru/streje/newspaper/repositories/RoleRepository.java
@@ -1,5 +1,11 @@
 package ru.streje.newspaper.repositories;
 
-public class RoleRepository {
+import java.util.Optional;
 
+import org.springframework.data.repository.CrudRepository;
+
+import ru.streje.newspaper.models.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+	Optional<Role> findByName(String name);
 }
