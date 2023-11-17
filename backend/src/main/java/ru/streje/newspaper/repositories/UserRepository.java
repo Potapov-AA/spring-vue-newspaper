efc72d00795f889f52d0de0345fe05b0284ee92b
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-17 15:26:11 +0300

    Добавлен метод поиска по email

--- backend/src/main/java/ru/streje/newspaper/repositories/UserRepository.java
+++ backend/src/main/java/ru/streje/newspaper/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package ru.streje.newspaper.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ru.streje.newspaper.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
+	Optional<User> findByEmail(String email);
+}	
