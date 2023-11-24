1be0ea8e8684a1b4ad524b1cdd151ae194d533c2
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-24 16:52:40 +0300

    Добавлены метод для заполнения коллекции тем; Обновлены методы добавления записи и обновления записи (учтено добавление тем)

--- backend/src/main/java/ru/streje/newspaper/services/ArticleService.java
+++ backend/src/main/java/ru/streje/newspaper/services/ArticleService.java
@@ -3,6 +3,7 @@ package ru.streje.newspaper.services;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -22,95 +23,129 @@ import ru.streje.newspaper.repositories.ArticleRepository;
 @RequiredArgsConstructor
 public class ArticleService {
 	private final ArticleRepository articleRepository;
-	
+
+	private final ThemeService themeService;
+
 	/**
 	 * Метод заполняет и возвращает экземпляр класса ArticleResponse
+	 * 
 	 * @param article - экземпляр класса Article
 	 * @return экземпляр класса ArticleResponse
 	 */
 	private ArticleResponse fillArticleResponse(Article article) {
 		ArticleResponse articleResponse = new ArticleResponse();
-		
+
 		articleResponse.setId(article.getId());
 		articleResponse.setTitle(article.getTitle());
 		articleResponse.setText(article.getText());
 		articleResponse.setImage(article.getImage());
 		articleResponse.setDate(article.getDate());
-		
+
 		List<String> themes = new ArrayList<>();
 		for (Theme theme : article.getThemes()) {
-			themes.add("#" + theme.getName());
-		}			
+			themes.add(theme.getName());
+		}
 		articleResponse.setThemes(themes);
-		
+
 		articleResponse.setCountLikes(article.getUsers().size());
-		
+
 		return articleResponse;
 	}
-	
+
 	/**
-	 * Метод возвращает все статьи при их наличии,
-	 * иначе возвращает сообщение, что статьи не найдены
+	 * Метод заполняет коллекцию тем
+	 * 
+	 * @param articleRequest - параметры req-запроса
+	 * @return Collection<Theme>
+	 */
+	private Collection<Theme> fillThemes(ArticleRequest articleRequest) {
+		Collection<Theme> themes = new ArrayList<>();
+		for (String themeName : articleRequest.getThemes()) {
+			if (themeService.getTheme(themeName) != null) {
+				Theme theme = themeService.getTheme(themeName);
+				themes.add(theme);
+			} else {
+				themeService.addTheme(themeName);
+				Theme theme = themeService.getTheme(themeName);
+				themes.add(theme);
+			}
+		}
+		return themes;
+	}
+
+	/**
+	 * Метод возвращает все статьи при их наличии, иначе возвращает сообщение, что
+	 * статьи не найдены
+	 * 
 	 * @return список всех статей (ArticleResponse) / сообщение об их отсутствии
 	 */
 	public ResponseEntity<?> getAllArticle() {
 		List<ArticleResponse> articles = new ArrayList<>();
-		
+
 		Iterable<Article> iArticles = articleRepository.findAll();
-		
+
 		for (Article article : iArticles) {
-			ArticleResponse articleResponse = fillArticleResponse(article);						
+			ArticleResponse articleResponse = fillArticleResponse(article);
 			articles.add(articleResponse);
 		}
-		
-		if(articles.size() > 0) {
+
+		if (articles.size() > 0) {
 			return new ResponseEntity<>(articles, HttpStatus.OK);
 		} else {
-			return new ResponseEntity<>(new ErrorMessage(HttpStatus.NOT_FOUND.value(), "Статьи не найдены"), HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(new ErrorMessage(HttpStatus.NOT_FOUND.value(), "Статьи не найдены"),
+					HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	/**
 	 * Метод поиска статьи по ID
+	 * 
 	 * @param id - индитификатор статьи
 	 * @return статью или сообщение о ее отсутствии
 	 */
 	public ResponseEntity<?> getArticle(int id) {
 		try {
 			Article article = articleRepository.findById(id).get();
-			
+
 			ArticleResponse articleResponse = fillArticleResponse(article);
-						
+
 			return new ResponseEntity<>(articleResponse, HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>(new ErrorMessage(HttpStatus.NOT_FOUND.value(), "Данная статья не найдена"), HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(new ErrorMessage(HttpStatus.NOT_FOUND.value(), "Данная статья не найдена"),
+					HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	/**
 	 * Метод добавления новой статьи в БД
+	 * 
 	 * @param articleRequest - данные для новой статьи
 	 * @return сообщение об успехе/провале добавления статьи
 	 */
 	public ResponseEntity<?> addNewArticle(ArticleRequest articleRequest) {
 		Article article = new Article();
-		
+
 		article.setTitle(articleRequest.getTitle());
 		article.setText(articleRequest.getText());
 		article.setImage(articleRequest.getImage());
 		article.setDate(Date.valueOf(LocalDate.now()));
-		
+
+		Collection<Theme> themes = fillThemes(articleRequest);
+		article.setThemes(themes);
+
 		try {
 			articleRepository.save(article);
 			return new ResponseEntity<>(new SuccesMessage("Статья успешно добавлена"), HttpStatus.OK);
 		} catch (Exception e) {
-			System.out.println(e);
-			return new ResponseEntity<>(new ErrorMessage(HttpStatus.BAD_REQUEST.value(), "Не удалось добавить новую статью"), HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>(
+					new ErrorMessage(HttpStatus.BAD_REQUEST.value(), "Не удалось добавить новую статью"),
+					HttpStatus.BAD_REQUEST);
 		}
 	}
-	
+
 	/**
 	 * Метод удаления статьи по ID
+	 * 
 	 * @param id - индитификатор статьи
 	 * @return сообщение об успехе/провале удаления статьи
 	 */
@@ -120,38 +155,48 @@ public class ArticleService {
 			articleRepository.delete(article);
 			return new ResponseEntity<>(new SuccesMessage("Статья успешно удалена"), HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>(new ErrorMessage(HttpStatus.NOT_FOUND.value(), "Данная статья не найдена или уже удалена"), HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(
+					new ErrorMessage(HttpStatus.NOT_FOUND.value(), "Данная статья не найдена или уже удалена"),
+					HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	/**
 	 * Метод обновления статьи по ID
+	 * 
 	 * @param articleRequest - изменяемые параметры
-	 * @param id - индитификатор статьи
+	 * @param id             - индитификатор статьи
 	 * @return сообщение об успехе/провале обновления статьи
 	 */
 	public ResponseEntity<?> changeAtricle(ArticleRequest articleRequest, int id) {
 		try {
 			Article article = articleRepository.findById(id).get();
-			
+
 			if (articleRequest.getTitle() != null) {
 				article.setTitle(articleRequest.getTitle());
 			}
-			
+
 			if (articleRequest.getText() != null) {
 				article.setText(articleRequest.getText());
 			}
-			
+
 			if (articleRequest.getImage() != null) {
 				article.setImage(articleRequest.getImage());
 			}
-			
+
+			if (articleRequest.getThemes() != null) {
+				Collection<Theme> themes = fillThemes(articleRequest);
+				article.setThemes(themes);
+			}
+
 			articleRepository.save(article);
-			
+
 			return new ResponseEntity<>(new SuccesMessage("Статья успешно изменена"), HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>(new ErrorMessage(HttpStatus.NOT_MODIFIED.value(), "Не удалось изменить данную статью"), HttpStatus.NOT_MODIFIED);
+			return new ResponseEntity<>(
+					new ErrorMessage(HttpStatus.NOT_MODIFIED.value(), "Не удалось изменить данную статью"),
+					HttpStatus.NOT_MODIFIED);
 		}
-		
+
 	}
 }
