b443519dda7cfb3cd6bb008182379d1cee8dd95f
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-24 14:53:54 +0300

    Добавлена валидация данных

--- backend/src/main/java/ru/streje/newspaper/dtos/ArticleRequest.java
+++ backend/src/main/java/ru/streje/newspaper/dtos/ArticleRequest.java
@@ -2,11 +2,16 @@ package ru.streje.newspaper.dtos;
 
 import java.sql.Date;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ArticleRequest {
+	@Size(min = 5, max = 255, message = "Название статьи должно состоять минимум из 5 и не более чем из 255 символов")
 	private String title;
+	
+	@Size(min = 20, message = "Текст статьи должен быть больше 20 символов")
 	private String text;
+	
 	private byte[] image;
 }
