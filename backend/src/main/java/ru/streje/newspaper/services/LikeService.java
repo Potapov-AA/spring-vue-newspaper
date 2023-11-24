c5cbe1fb808084331bba93a798bff9e0bc7d6ab9
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-24 19:03:27 +0300

    Добавлен метод получения статуса лайка пользоватлея по ID статьи

--- backend/src/main/java/ru/streje/newspaper/services/LikeService.java
+++ backend/src/main/java/ru/streje/newspaper/services/LikeService.java
@@ -18,29 +18,63 @@ public class LikeService {
 	private final ArticleService articleService;
 	private final UserService userService;
 	private final JwtTokenUtils jwtTokenUtils;
-	
+
+	/**
+	 * Метод добавления/удаления лайков к статье
+	 * 
+	 * @param token     - токен авторизации, для получения данных о пользователе
+	 * @param articleId - индитификатор статьи
+	 * @return LikeRespone(countLike:int, userStatus:int)
+	 */
 	public ResponseEntity<?> addRemoveLike(String token, int articleId) {
 		LikeResponse likeResponse = new LikeResponse();
-		
+
 		Article article = articleService.getArticle(articleId);
 		Collection<User> users = article.getUsers();
-		
+
 		String email = jwtTokenUtils.getUsername(token);
 		User targetUser = userService.findByEmail(email).get();
-		
-		if(users.contains(targetUser)) {
+
+		if (users.contains(targetUser)) {
 			users.remove(targetUser);
 			likeResponse.setCountLike(users.size());
-			likeResponse.setUserStatus(-1);					
+			likeResponse.setUserStatus(-1);
 		} else {
 			users.add(targetUser);
 			likeResponse.setCountLike(users.size());
-			likeResponse.setUserStatus(1);			
+			likeResponse.setUserStatus(1);
 		}
-		
+
 		article.setUsers(users);
 		articleService.saveArticle(article);
-		
+
+		return new ResponseEntity<>(likeResponse, HttpStatus.OK);
+	}
+
+	/**
+	 * Метод получения статуса лайка пользователя
+	 * 
+	 * @param token     - токен авторизации, для получения данных о пользователе
+	 * @param articleId - индитификатор статьи
+	 * @return LikeRespone(countLike:int, userStatus:int)
+	 */
+	public ResponseEntity<?> getUserLikeStatus(String token, int articleId) {
+		LikeResponse likeResponse = new LikeResponse();
+
+		Article article = articleService.getArticle(articleId);
+		Collection<User> users = article.getUsers();
+
+		String email = jwtTokenUtils.getUsername(token);
+		User targetUser = userService.findByEmail(email).get();
+
+		likeResponse.setCountLike(users.size());
+
+		if (users.contains(targetUser)) {
+			likeResponse.setUserStatus(1);
+		} else {
+			likeResponse.setUserStatus(-1);
+		}
+
 		return new ResponseEntity<>(likeResponse, HttpStatus.OK);
 	}
 }
