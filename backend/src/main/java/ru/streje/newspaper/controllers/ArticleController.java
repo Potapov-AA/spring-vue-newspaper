da99e91f4dde196dfbd2825afa5fe63c8a226e3d
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-24 12:26:14 +0300

    Создан контроллер для работы со статьями

--- backend/src/main/java/ru/streje/newspaper/controllers/ArticleController.java
+++ backend/src/main/java/ru/streje/newspaper/controllers/ArticleController.java
@@ -4,20 +4,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import ru.streje.newspaper.dtos.ArticleRequest;
 import ru.streje.newspaper.services.ArticleService;
 
 @CrossOrigin
 @RestController
 @RequiredArgsConstructor
-public class MainController {
+public class ArticleController {
 	final private ArticleService articleService;
-	
+
 	@GetMapping("/")
-	public ResponseEntity<?> getArticle(@RequestHeader("authorization") String token) {
-		return new ResponseEntity<>(articleService.getAllArticle(), HttpStatus.OK);
+	public ResponseEntity<?> getArticles() {
+		return articleService.getAllArticle();
+	}
+	
+	@PostMapping("/newarticle")
+	public ResponseEntity<?> postNewArticle(@RequestBody ArticleRequest articleRequest) {
+		return new ResponseEntity<>(articleService.addNewArticle(articleRequest), HttpStatus.OK);
 	}
 }
