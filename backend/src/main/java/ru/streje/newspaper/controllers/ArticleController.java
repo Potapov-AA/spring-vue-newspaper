cc789f8f6f6e1f22cb6005c47be31b37f1953f84
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-24 14:13:09 +0300

    Добавлен delete-запрос для удаления статьи по id

--- backend/src/main/java/ru/streje/newspaper/controllers/ArticleController.java
+++ backend/src/main/java/ru/streje/newspaper/controllers/ArticleController.java
@@ -3,9 +3,12 @@ package ru.streje.newspaper.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -24,7 +27,12 @@ public class ArticleController {
 	}
 	
 	@PostMapping("/newarticle")
-	public ResponseEntity<?> postNewArticle(@RequestBody ArticleRequest articleRequest) {
+	public ResponseEntity<?> postArticle(@RequestBody ArticleRequest articleRequest) {
 		return articleService.addNewArticle(articleRequest);
+	}
+	
+	@DeleteMapping("/deletearticle/{id}")
+	public ResponseEntity<?> deleteArticle(@PathVariable("id") Integer id) {
+		return articleService.deleteArticle(id);
 	}
 }
