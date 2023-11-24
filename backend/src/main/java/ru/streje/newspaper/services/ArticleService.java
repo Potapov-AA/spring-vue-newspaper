9b1672ec0f560be50428057ce29ec38ae4cfcbff
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-24 12:29:05 +0300

    Исправлен метод получения всех статьей, учтена возможность получения отсутствия статьей

--- backend/src/main/java/ru/streje/newspaper/services/ArticleService.java
+++ backend/src/main/java/ru/streje/newspaper/services/ArticleService.java
@@ -1,12 +1,19 @@
 package ru.streje.newspaper.services;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import ru.streje.newspaper.dtos.ArticleResponse;
+import ru.streje.newspaper.dtos.ArticleRequest;
+import ru.streje.newspaper.dtos.ArticlesResponse;
+import ru.streje.newspaper.messages.ErrorMessage;
+import ru.streje.newspaper.messages.SuccesMessage;
 import ru.streje.newspaper.models.Article;
 import ru.streje.newspaper.models.Theme;
 import ru.streje.newspaper.repositories.ArticleRepository;
@@ -16,13 +23,17 @@ import ru.streje.newspaper.repositories.ArticleRepository;
 public class ArticleService {
 	private final ArticleRepository articleRepository;
 	
-	public List<ArticleResponse> getAllArticle() {
-		List<ArticleResponse> articles = new ArrayList<>();
+	/**
+	 * Метод возвращает все статьи
+	 * @return список всех статей (ArticleResponse)
+	 */
+	public ResponseEntity<?> getAllArticle() {
+		List<ArticlesResponse> articles = new ArrayList<>();
 		
 		Iterable<Article> iArticles = articleRepository.findAll();
 		
 		for (Article article : iArticles) {
-			ArticleResponse articleRes = new ArticleResponse();
+			ArticlesResponse articleRes = new ArticlesResponse();
 			articleRes.setId(article.getId());
 			articleRes.setTitle(article.getTitle());
 			articleRes.setText(article.getText());
@@ -40,6 +51,22 @@ public class ArticleService {
 			articles.add(articleRes);
 		}
 		
-		return articles;
+		if(articles.size() > 0) {
+			return new ResponseEntity<>(articles, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(new ErrorMessage(HttpStatus.NOT_FOUND.value(), "Статьи не найдены"), HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	public SuccesMessage addNewArticle(ArticleRequest articleRequest) {
+		Article article = new Article();
+		
+		article.setTitle(articleRequest.getTitle());
+		article.setText(articleRequest.getText());
+		article.setImage(articleRequest.getImage());
+		article.setDate(Date.valueOf(LocalDate.now()));
+		
+		articleRepository.save(article);
+		return new SuccesMessage("Article added successfully");
 	}
 }
