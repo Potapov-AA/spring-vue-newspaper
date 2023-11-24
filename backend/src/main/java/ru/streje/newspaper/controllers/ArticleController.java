784f978a902e00885a9e8859e1ac9be0cca27655
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-24 14:56:46 +0300

    Добавлен put-запрос для изменения статьи

--- backend/src/main/java/ru/streje/newspaper/controllers/ArticleController.java
+++ backend/src/main/java/ru/streje/newspaper/controllers/ArticleController.java
@@ -1,16 +1,16 @@
 package ru.streje.newspaper.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import ru.streje.newspaper.dtos.ArticleRequest;
 import ru.streje.newspaper.services.ArticleService;
@@ -27,12 +27,17 @@ public class ArticleController {
 	}
 	
 	@PostMapping("/newarticle")
-	public ResponseEntity<?> postArticle(@RequestBody ArticleRequest articleRequest) {
+	public ResponseEntity<?> postArticle(@Valid @RequestBody ArticleRequest articleRequest) {
 		return articleService.addNewArticle(articleRequest);
 	}
 	
 	@DeleteMapping("/deletearticle/{id}")
 	public ResponseEntity<?> deleteArticle(@PathVariable("id") Integer id) {
 		return articleService.deleteArticle(id);
+	}
+	
+	@PutMapping("/changearticle/{id}")
+	public ResponseEntity<?> putArticle(@Valid @RequestBody ArticleRequest articleRequest, @PathVariable("id") Integer id) {
+		return articleService.changeAtricle(articleRequest, id);
 	}
 }
