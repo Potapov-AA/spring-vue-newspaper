76e360b5533758748e7e46f572b19d62dbe9ff57
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-24 19:19:53 +0300

    Создан сервис бизнес-логики для работы с комментариями

---
+++ backend/src/main/java/ru/streje/newspaper/services/CommentService.java
@@ -0,0 +1,12 @@
+package ru.streje.newspaper.services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import ru.streje.newspaper.utilis.JwtTokenUtils;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+}
