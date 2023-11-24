01753072147e5ab558bcc3831ec523db906a299f
Alexandr Potapov <San_sanih99@mail.ru>
2023-11-24 20:23:10 +0300

    Прописаны все мапинги для комментариев

--- backend/src/main/java/ru/streje/newspaper/controllers/CommentController.java
+++ backend/src/main/java/ru/streje/newspaper/controllers/CommentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +20,13 @@ public class CommentController {
 	private final CommentService commentService;
 	
 	@GetMapping("/comments/{articleId}")
-	public ResponseEntity<?> getComments(@RequestHeader("authorization") String token, @PathVariable("articleId") Integer articleId) {
-		return null;
+	public ResponseEntity<?> getComments(@PathVariable("articleId") Integer articleId) {
+		return commentService.getComments(articleId);
 	}
 	
-	@PostMapping("/addcomment/{articleId")
-	public ResponseEntity<?> postComment(@RequestHeader("authorization") String token, @PathVariable("articleId") Integer articleId, CommentRequest commentRequest) {
-		return null;
+	@PostMapping("/addcomment/{articleId}")
+	public ResponseEntity<?> postComment(@RequestHeader("authorization") String token, @PathVariable("articleId") Integer articleId, @RequestBody CommentRequest commentRequest) {
+		return commentService.addComment(token.substring(7), articleId, commentRequest);
 	}
 	
 }
