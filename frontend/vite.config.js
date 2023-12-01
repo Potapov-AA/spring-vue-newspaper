56c9109a46a2803318a43b9b14d795b0e82f1f7f
Alexandr Potapov <San_sanih99@mail.ru>
2023-12-01 15:57:32 +0300

    Добавлена конфигурация сервера

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,14 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: true,
+    strictPort: true,
+    port: 8000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
