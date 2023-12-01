288d0232dd6ef907264181b4d7bfabe82d89693f
Alexandr Potapov <San_sanih99@mail.ru>
2023-12-01 20:35:29 +0300

    Настроена шапка

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -3,18 +3,21 @@ import { useTokenStore } from '@/stores/token'
 </script>
 
 <template>
-  <div>
-    <p class="title"><b>The Daily Bugle</b></p>
-
-    <div v-if="useTokenStore().logined">
-      <v-btn type="button">Выход</v-btn>
+  <v-sheet width="100%" :elevation="4" class="pa-6">
+    <div class="d-flex align-center justify-space-between">
+      <p class="title"><b>The Daily Bugle</b></p>
+      <div v-if="useTokenStore().logined" class="d-flex align-center">
+        <div class="mr-4">
+          {{ useTokenStore().firstname }}
+          {{ useTokenStore().lastname }}
+        </div>
+        <v-btn @click="useTokenStore().forgetToken()" type="button">Выход</v-btn>
+      </div>
+      <div v-else>
+        <v-btn class="mx-2"><router-link :to="{ name: 'login' }" class="link">Войти</router-link></v-btn>
+        <v-btn><router-link :to="{ name: 'reg' }" class="link">Зарегестрироваться</router-link></v-btn>
+        
+      </div>
     </div>
-    <div v-else>
-        <router-link :to="{ name: 'login' }">Войти</router-link>
-        <router-link :to="{ name: 'reg' }">Зарегестрироваться</router-link>
-    </div>
-  </div>
+  </v-sheet>
 </template>
-
-
-<style></style>
