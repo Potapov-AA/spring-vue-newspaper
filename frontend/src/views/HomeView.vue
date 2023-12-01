4cd8d72dfa4b284a13dbfc477e0dae7c08a15fe8
Alexandr Potapov <San_sanih99@mail.ru>
2023-12-01 18:40:39 +0300

    Рефакторинг и перенос шапки в отдельный компонент

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,33 @@
+<script setup>
+import { useArticleStore } from '@/stores/articles'
+import { onMounted } from 'vue'
+
+import HeaderComponent from '@/components/HeaderComponent.vue'
+
+onMounted(() => {
+    console.log(1)
+    useArticleStore().getArticles()
+})
+
+</script>
+
 <template>
-  <div>ГЛАВНАЯ</div>
+  <v-container>
+    <HeaderComponent/>
+
+
+    <div v-if="useArticleStore().articles.length > 0">
+        <div v-for="article in useArticleStore().articles" :key="article.id">
+            {{ article }}
+            {{ article.date.substring(0, 10) }}
+        </div>
+    </div>
+    <div v-else>
+        {{ useArticleStore().errorMessage }}
+    </div>
+  </v-container>
 </template>
 
-<script>
-export default {}
-</script>
+
 
 <style></style>
