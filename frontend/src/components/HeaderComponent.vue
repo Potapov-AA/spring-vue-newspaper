468df6af3d15c96a684da6b556609d3f5256f3a8
Alexandr Potapov <San_sanih99@mail.ru>
2023-12-01 18:39:31 +0300

    Перенес заголовок в отдельный компонент

---
+++ frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useTokenStore } from '@/stores/token'
+</script>
+
+<template>
+  <div>
+    <p class="title"><b>The Daily Bugle</b></p>
+
+    <div v-if="useTokenStore().logined">
+      <v-btn type="button">Выход</v-btn>
+    </div>
+    <div v-else>
+        <router-link :to="{ name: 'login' }">Войти</router-link>
+        <router-link :to="{ name: 'reg' }">Зарегестрироваться</router-link>
+    </div>
+  </div>
+</template>
+
+
+<style></style>
