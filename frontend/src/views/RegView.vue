bd3c271c0335fdd10137054f58c5f7cf2f04b885
Alexandr Potapov <San_sanih99@mail.ru>
2023-12-01 16:10:37 +0300

    Создана страница регистрации нового пользователя

---
+++ frontend/src/views/RegView.vue
@@ -0,0 +1,85 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const responseMessage = ref('')
+const isSuccess = ref(false)
+
+const firstname = ref('')
+const lastname = ref('')
+const email = ref('')
+const password = ref('')
+const confirmPassword = ref('')
+
+async function registration(firstname, lastname, email, password, confPassword) {
+  await axios
+    .post('http://localhost:8081/api/registration', {
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      password: password,
+      confirmPassword: confPassword,
+      
+    })
+    .then((response) => {
+      {
+        responseMessage.value = response.data.message
+        isSuccess.value = true
+      }
+    })
+    .catch((error) => {
+      responseMessage.value = error.response.data.message
+      isSuccess.value = false
+    })
+}
+</script>
+
+<template>
+  <v-container class="d-flex fill-height fluid justify-center">
+    <v-sheet :elevation="9" :width="500" class="mx-auto rounded-lg pa-6">
+      <v-form>
+        <h2 align="center" class="mb-3">РЕГИСТРАЦИЯ</h2>
+
+        <v-text-field 
+          v-model="firstname" 
+          type="text" 
+          label="Имя" 
+          placeholder="Иван" 
+        />
+        <v-text-field 
+          v-model="lastname" 
+          type="text" 
+          label="Фамилия" 
+          placeholder="Иванов" 
+        />
+        <v-text-field 
+          v-model="email" 
+          type="email" 
+          label="Почта" 
+          placeholder="johndoe@gmail.com" 
+        />
+        <v-text-field
+          v-model="password"
+          type="password"
+          label="Пароль"
+          placeholder="Введите пароль"
+        />
+        <v-text-field
+          v-model="confirmPassword"
+          type="password"
+          label="Подтвердите пароль"
+          placeholder="Подтвердите пароль"
+        />
+
+        <p align="center" :class="{'errorMessage': !isSuccess, 'successMessage': isSuccess, 'mb-4': true}">{{ responseMessage }}</p>
+
+        <v-btn @click="registration(firstname, lastname, email, password, confirmPassword)" block class="mt-2">Войти</v-btn>
+
+        <div class="mt-4 d-flex justify-space-between">
+            <router-link :to="{ name: 'login' }">Назад</router-link>
+            <router-link :to="{ name: 'home' }">На главную</router-link>
+          </div>
+      </v-form>
+    </v-sheet>
+  </v-container>
+</template>
