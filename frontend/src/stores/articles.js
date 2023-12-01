d91895edb29b6510a294f7aaf136cfc6d765c7fc
Alexandr Potapov <San_sanih99@mail.ru>
2023-12-01 18:39:50 +0300

    Хранилище для работы со статьями

---
+++ frontend/src/stores/articles.js
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+const STORE_NAME = 'articles'
+
+export const useArticleStore = defineStore(STORE_NAME, {
+  state: () => ({
+    articles: [],
+    errorMessage: ''
+  }),
+
+  actions: {
+    async getArticles() {
+        await axios
+          .get('http://localhost:8081/api/articles')
+          .then((response) => {
+            {   
+                this.articles = response.data.reverse()
+                this.errorMessage = ''
+            }
+          })
+          .catch((error) => {
+            this.errorMessage = error.response.data.message
+          })
+      }
+  }
+})
