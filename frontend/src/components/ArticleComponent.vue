a822d382f7918c674a85dfc1c1cc2b364dbf5ceb
Alexandr Potapov <San_sanih99@mail.ru>
2023-12-08 20:53:13 +0300

    Создан компонент для визуализации статьи

---
+++ frontend/src/components/ArticleComponent.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  article: Object
+})
+
+const date = ref(new Date(props.article.date))
+</script>
+
+<template>
+  <!-- <h2>{{ props.article }}</h2> -->
+  <v-card class="d-flex pa-4 border solid">
+    <div>
+      <img v-if="props.article.image === null" src="/default-news.png" width="200" height="200" />
+      <img v-else src="/default-news.png" width="200" height="200" />
+    </div>
+    <div>
+      <div class="d-flex">
+        <b v-for="tag in props.article.themes" :key="tag" class="mx-1"
+          >#<i>{{ tag }}</i></b
+        >
+      </div>
+      <h2>{{ props.article.title }}</h2>
+      <div>
+        <span class="d-inline-block text-truncate" style="max-width: 500px;">{{ props.article.text }}</span>
+      </div>
+      <p align="right">
+        {{ date.getDate() }}-{{ date.getMonth() + 1 }}-{{ date.getFullYear() }}
+        {{ date.getHours() }}:{{ date.getMinutes() }}
+      </p>
+    </div>
+  </v-card>
+</template>
