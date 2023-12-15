4ae0c3fc97c6bd7801f47eb76b8bff7bb81a1f83
Alexandr Potapov <San_sanih99@mail.ru>
2023-12-15 17:26:55 +0300

    Добавлена возмоность раскрывать и скрывать текст статьи

--- frontend/src/components/ArticleComponent.vue
+++ frontend/src/components/ArticleComponent.vue
@@ -1,16 +1,35 @@
 <script setup>
 import { ref } from 'vue'
 
+import LikeComponent from '@/components/LikeComponent.vue'
+
 const props = defineProps({
   article: Object
 })
 
 const date = ref(new Date(props.article.date))
+
+function showMoreContent(id) {
+  var article = document.getElementById('article-' + id)
+  var btnShow = document.getElementById('btn-show-' + id)
+  var btnHide = document.getElementById('btn-hide-' + id)
+  article.className = ''
+  btnHide.className = 'v-btn border pa-2'
+  btnShow.className = 'hidden'
+}
+
+function hideContent(id) {
+  var article = document.getElementById('article-' + id)
+  var btnShow = document.getElementById('btn-show-' + id)
+  var btnHide = document.getElementById('btn-hide-' + id)
+  article.className = 'hidden '
+  btnHide.className = 'hidden'
+  btnShow.className = 'v-btn border pa-2'
+}
 </script>
 
 <template>
-  <!-- <h2>{{ props.article }}</h2> -->
-  <v-card class="d-flex pa-4 border solid">
+  <v-card class="d-flex pt-3 px-2 border solid">
     <div>
       <img v-if="props.article.image === null" src="/default-news.png" width="200" height="200" />
       <img v-else src="/default-news.png" width="200" height="200" />
@@ -23,12 +42,34 @@ const date = ref(new Date(props.article.date))
       </div>
       <h2>{{ props.article.title }}</h2>
       <div>
-        <span class="d-inline-block text-truncate" style="max-width: 500px;">{{ props.article.text }}</span>
+        {{ props.article.text.slice(0, 150) }}
+        <span :id="'article-' + props.article.id" class="hidden">{{
+          props.article.text.slice(150)
+        }}</span>
       </div>
-      <p align="right">
-        {{ date.getDate() }}-{{ date.getMonth() + 1 }}-{{ date.getFullYear() }}
-        {{ date.getHours() }}:{{ date.getMinutes() }}
-      </p>
+      <div class="my-3 mr-2 d-flex align-center justify-space-between">
+        <button
+          :id="'btn-show-' + props.article.id"
+          @click="showMoreContent(props.article.id)"
+          class="v-btn border pa-2"
+        >
+          Показать больше
+        </button>
+        <button
+          :id="'btn-hide-' + props.article.id"
+          @click="hideContent(props.article.id)"
+          class="hidden"
+        >
+          Показать меньше
+        </button>
+        <div class="d-flex align-center">
+          <p align="right" class="mr-2">
+            {{ date.getDate() }}-{{ date.getMonth() + 1 }}-{{ date.getFullYear() }}
+            {{ date.getHours() }}:{{ date.getMinutes() }}
+          </p>
+          <LikeComponent/>
+        </div>
+      </div>
     </div>
   </v-card>
 </template>
