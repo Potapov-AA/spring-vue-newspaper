a8c8ca6b75ad75e2b9d7a426635ed63fe8b5b2c7
Alexandr Potapov <San_sanih99@mail.ru>
2023-12-25 10:02:15 +0300

    Добавлена функция проверки статуса токена

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
+import axios from 'axios'
 import { useArticleStore } from '@/stores/articles'
+import { useTokenStore } from '../stores/token'
 import { onMounted, ref } from 'vue'
 
 import HeaderComponent from '@/components/HeaderComponent.vue'
-
 import PaginationComponent from '@/components/PaginationComponent.vue'
 
 const isArticles = ref(false)
@@ -13,18 +14,34 @@ function updateArticle() {
   isArticles.value = useArticleStore().articles.length > 0 ? true : false
 }
 
-onMounted(async() => {
+async function checkToken() {
+  await axios({
+    url: 'http://localhost:8081/api/checktoken',
+    method: 'post',
+    headers: {
+      Authorization: useTokenStore().token
+    }
+  })
+    .catch(() => {
+      useTokenStore().forgetToken()
+    })
+}
+
+onMounted(async () => {
+  checkToken()
   await useArticleStore().getArticles()
   isArticles.value = useArticleStore().articles.length > 0 ? true : false
-  window.setInterval(updateArticle, 30000)
+
+  window.setInterval(updateArticle, 60000)
+  window.setInterval(checkToken, 60000)
 })
 </script>
 
 <template>
   <v-container class="d-flex fill-height fluid justify-start align-center flex-column">
-    <HeaderComponent/>
-    <div v-if="isArticles">      
-      <PaginationComponent v-bind:items="useArticleStore().articles"/>
+    <HeaderComponent />
+    <div v-if="isArticles">
+      <PaginationComponent v-bind:items="useArticleStore().articles" />
     </div>
     <div v-else class="py-10 px-5">
       {{ useArticleStore().errorMessage }}
