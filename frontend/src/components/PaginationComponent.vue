560dd361e8cc4d2cfa00c5dab1b1e87e3139054d
Alexandr Potapov <San_sanih99@mail.ru>
2024-01-19 21:31:03 +0300

    Настроен начальный вид блока тематик

--- frontend/src/components/PaginationComponent.vue
+++ frontend/src/components/PaginationComponent.vue
@@ -1,5 +1,7 @@
 <script setup>
+import { useTokenStore, ROLES } from '@/stores/token'
 import ArticleComponent from '@/components/ArticleComponent.vue'
+import ThemeListComponent from '@/components/ThemeListComponent.vue'
 import { computed, ref } from 'vue'
 
 const props = defineProps({
@@ -32,14 +34,19 @@ const paginatedData = computed(() => {
 
 <template>
   <div class="py-5 px-5">
-    <div class="d-flex align-center justify-center mb-4">
-      <v-btn @click="prevPage" :disabled="pageNumber === 0">
-        Назад
-      </v-btn>
-      <p class="mx-5">{{ pageNumber + 1 }}</p>
-      <v-btn @click="nextPage" :disabled="pageNumber === pageCount-1">
-        Далее
-      </v-btn>
+    <div class="d-flex justify-space-left align-top">
+      <ThemeListComponent v-if="useTokenStore().role == ROLES.USER" class="mb-4"/>
+      <v-btn class="mb-4 mx-4">Скрыть не понравившиеся темы</v-btn>
+      <v-btn class="mb-4 mx-4">Только избранные темы</v-btn>
+      <div class="d-flex  justify-center mb-4">
+        <v-btn @click="prevPage" :disabled="pageNumber === 0">
+          Назад
+        </v-btn>
+        <p class="mx-5 mt-1 text-h5">{{ pageNumber + 1 }}</p>
+        <v-btn @click="nextPage" :disabled="pageNumber === pageCount-1">
+          Далее
+        </v-btn>
+      </div>
     </div>
     <div v-for="item in paginatedData" :key="item.id" class="mb-4">
       <ArticleComponent v-bind:article="item" />
