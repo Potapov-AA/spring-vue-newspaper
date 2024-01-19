ac887bb0ce326e3e1ed632df23515ec2c1889c9e
Alexandr Potapov <San_sanih99@mail.ru>
2024-01-19 19:23:03 +0300

    Добавлены функции для работы с понравившимся и не понравившимися темами

--- backend/src/main/java/ru/streje/newspaper/services/ThemeService.java
+++ backend/src/main/java/ru/streje/newspaper/services/ThemeService.java
@@ -1,15 +1,29 @@
 package ru.streje.newspaper.services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import ru.streje.newspaper.models.LikeDislikeTheme;
 import ru.streje.newspaper.models.Theme;
+import ru.streje.newspaper.models.User;
+import ru.streje.newspaper.repositories.LikeDislikeThemeRepository;
 import ru.streje.newspaper.repositories.ThemeRepository;
+import ru.streje.newspaper.utilis.JwtTokenUtils;
 
 @Service
 @RequiredArgsConstructor
 public class ThemeService {
 	private final ThemeRepository themeRepository;
+	private final LikeDislikeThemeRepository likeDislikeThemeRepository;
+	private final JwtTokenUtils jwtTokenUtils;
+	private final UserService userService;
 
 	/**
 	 * Метод получения экземпляра Theme по переданому имени, если темы с данным
@@ -37,5 +51,86 @@ public class ThemeService {
 		theme.setName(name);
 
 		themeRepository.save(theme);
+	}
+	
+	
+	public ResponseEntity<?> getAllTheme() {
+		Iterable<Theme> itThemes = themeRepository.findAll(); 
+		
+		List<Theme> themes  =  new ArrayList<>();
+		for(Theme theme: itThemes) {
+			themes.add(theme);
+		}
+		
+		if (themes.size() > 0) {
+			return new ResponseEntity<>(themes, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	
+	public ResponseEntity<?> getUserLikesDislikeTheme(String token, Integer status) {
+		String email = jwtTokenUtils.getUsername(token);
+		User user = userService.findByEmail(email).get();
+		Collection<LikeDislikeTheme> likeDislikeTheme = likeDislikeThemeRepository.findByUser(user);
+
+		List<Theme> themes =  new ArrayList<>();
+		for(LikeDislikeTheme theme: likeDislikeTheme) {
+			if(theme.getStatus() == status) {
+				themes.add(theme.getTheme());
+			}
+		}
+		
+		if (themes.size() > 0) {
+			return new ResponseEntity<>(themes, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(
+					HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	public ResponseEntity<?> deleteUserLikeDislikeTheme(String token, Integer themeId) {
+		String email = jwtTokenUtils.getUsername(token);
+		User user = userService.findByEmail(email).get();
+		
+		try {
+			Theme theme = themeRepository.findById(themeId).get();
+			LikeDislikeTheme likeDislikeThemes = likeDislikeThemeRepository.findByUserAndTheme(user, theme).get();
+			likeDislikeThemeRepository.delete(likeDislikeThemes);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	public ResponseEntity<?> addUserLikeDislikeTheme(String token, Integer themeId, Integer status) {
+		String email = jwtTokenUtils.getUsername(token);
+		User user = userService.findByEmail(email).get();
+		Theme theme;
+		
+		try {
+			theme = themeRepository.findById(themeId).get();	
+		} catch (NoSuchElementException e) {
+			return new ResponseEntity<>("Данной тематики не существует", HttpStatus.BAD_REQUEST);
+		}
+		
+		try {
+			LikeDislikeTheme likeDislikeThemes = likeDislikeThemeRepository.findByUserAndTheme(user, theme).get();
+			likeDislikeThemeRepository.delete(likeDislikeThemes);
+			likeDislikeThemes.setStatus(status);
+			likeDislikeThemeRepository.save(likeDislikeThemes);
+			
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (NoSuchElementException e) {
+			LikeDislikeTheme likeDislikeThemes = new LikeDislikeTheme();
+			likeDislikeThemes.setStatus(status);
+			likeDislikeThemes.setTheme(theme);
+			likeDislikeThemes.setUser(user);
+			
+			likeDislikeThemeRepository.save(likeDislikeThemes);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
 	}
 }
