c31761b85e9852f2c2a452e596c990bcf4b0b786
Alexandr Potapov <San_sanih99@mail.ru>
2024-01-19 21:31:15 +0300

    Сделан выпадающий список тем

---
+++ frontend/src/components/ThemeListComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useThemeStore } from '@/stores/themes'
+import { useTokenStore } from '@/stores/token'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  useThemeStore().getThemes(useTokenStore().token)
+})
+</script>
+
+<template>
+  <v-expansion-panels class="h-25">
+    <v-expansion-panel class="h-25">
+      <v-expansion-panel-title> Темы </v-expansion-panel-title>
+      <v-expansion-panel-text v-for="theme in useThemeStore().themes" :key="theme.id">
+        <b >
+            {{ theme.name }}
+        </b>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
