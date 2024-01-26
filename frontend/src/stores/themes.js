1b83ddeb4cbe8c005f3d4575e33800a26d439ab4
Alexandr Potapov <San_sanih99@mail.ru>
2024-01-26 14:53:41 +0300

    Добавлены методы для взаимодействия с темами

--- frontend/src/stores/themes.js
+++ frontend/src/stores/themes.js
@@ -6,28 +6,100 @@ const STORE_NAME = 'themes'
 export const useThemeStore = defineStore(STORE_NAME, {
   state: () => ({
     themes: [],
-    errorMessage: '', // TODO поменять имя на просто message
+    errorMessage: '' // TODO поменять имя на просто message
   }),
 
   actions: {
     async getThemes(token) {
-        await axios({
-            url: 'http://localhost:8081/api/allthemes', 
-            method: 'get',
-            headers: {
-              Authorization: token
-            }
+      await axios({
+        url: 'http://localhost:8081/api/allthemes',
+        method: 'get',
+        headers: {
+          Authorization: token
+        }
+      })
+        .then((response) => {
+          {
+            this.themes = response.data
+            this.errorMessage = ''
+          }
         })
-            .then((response) => {
-            {
-                this.themes = response.data
-                this.errorMessage = ''
-            }
-            })
-            .catch((error) => {
-            this.errorMessage = error.response.data.message
-            this.themes = []
-            })
-        },
-  },
+        .catch((error) => {
+          this.errorMessage = error.response.data.message
+          this.themes = []
+        })
+    },
+
+    async checkStatusThemes(token, themeId) {
+      let status = 0
+
+      await axios({
+        url: 'http://localhost:8081/api/checkstatus/'+themeId,
+        method: 'get',
+        headers: {
+          Authorization: token
+        }
+      })
+        .then((response) => {
+          {
+            status = response.data
+          }
+        })
+        .catch(() => {
+          status = 0
+        })
+      
+        return status
+    },
+
+    async addLikeTheme(token, themeId) {
+      await axios({
+        url: 'http://localhost:8081/api/addliketheme/'+themeId,
+        method: 'post',
+        headers: {
+          Authorization: token
+        }
+      })
+    },
+
+    async addDislikeTheme(token, themeId) {
+      await axios({
+        url: 'http://localhost:8081/api/adddisliketheme/'+themeId,
+        method: 'post',
+        headers: {
+          Authorization: token
+        }
+      })
+    },
+
+    async removeLikeDislikeTheme(token, themeId) {
+      await axios({
+        url: 'http://localhost:8081/api/deletelikedislikethemes/'+themeId,
+        method: 'delete',
+        headers: {
+          Authorization: token
+        }
+      })
+    },
+
+    async getDislikeThemes(token) {
+      let dislikeThemes = []
+
+      await axios({
+        url: 'http://localhost:8081/api/dislikethemes',
+        method: 'get',
+        headers: {
+          Authorization: token
+        }
+      })
+        .then((response) => {
+          dislikeThemes = response.data
+        })
+        .catch(() => {
+          dislikeThemes = []
+        })
+
+      return dislikeThemes
+    }
+  }
 })
