c9c37b730979d93572bd436c07a26aa7029d7079
Alexandr Potapov <San_sanih99@mail.ru>
2024-01-26 16:47:36 +0300

    Перенес функцию обновления статьей в function.js

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,40 +1,30 @@
 <script setup>
-import { useThemeStore } from '@/stores/themes'
+import { updateListArticles } from '@/js/functions.js'
+
 import { useArticleStore } from '@/stores/articles'
-import { useTokenStore } from '../stores/token'
-import { onMounted, ref } from 'vue'
+import { useTokenStore } from '@/stores/token'
+import { onMounted} from 'vue'
 import HeaderComponent from '@/components/HeaderComponent.vue'
 import PaginationComponent from '@/components/PaginationComponent.vue'
 
-const isArticles = ref(false)
-
-async function getArticles() {
-  
-  let themes = await useThemeStore().getDislikeThemes(useTokenStore().token)
-  await useArticleStore().getArticles(themes)
-
-  isArticles.value = useArticleStore().articles.length > 0 ? true : false
-}
 
 onMounted(async () => {
   await useTokenStore().checkTokenStatus()
-  await getArticles()
+  await updateListArticles()
 
-  isArticles.value = useArticleStore().articles.length > 0 ? true : false
-
-  window.setInterval(await getArticles, 10000)
-  window.setInterval(await useTokenStore().checkTokenStatus(), 60000)
+  window.setInterval(await useTokenStore().checkTokenStatus, 60000)
+  window.setInterval(await updateListArticles, 60000)
 })
 </script>
 
 <template>
   <v-container fluid class="d-flex fill-height fluid justify-start align-center flex-column bck-img">
     <HeaderComponent />
-    <div v-if="isArticles">
+    <div v-if="useArticleStore().httpStatus != 500">  
       <PaginationComponent v-bind:items="useArticleStore().articles" />
     </div>
     <div v-else class="py-10 px-5">
-      {{ useArticleStore().errorMessage }}
+      {{ useArticleStore().message }}
     </div>
   </v-container>
 </template>
