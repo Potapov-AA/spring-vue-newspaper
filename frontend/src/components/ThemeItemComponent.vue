30cdc417e317b3fae83d3c362d23bc817d58286c
Alexandr Potapov <San_sanih99@mail.ru>
2024-01-26 14:57:52 +0300

    Прописан компонент для элемента выпадающего списка тем

---
+++ frontend/src/components/ThemeItemComponent.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { useThemeStore } from '@/stores/themes'
+import { useTokenStore } from '@/stores/token'
+import { onMounted, ref } from 'vue'
+
+const status = ref(0)
+
+const props = defineProps({
+  theme: Object
+})
+
+
+async function addRemoveLikeTheme() {
+    if(status.value == 1) {
+        await useThemeStore().removeLikeDislikeTheme(useTokenStore().token, props.theme.id)
+        status.value = await useThemeStore().checkStatusThemes(useTokenStore().token, props.theme.id)
+    } else {
+        await useThemeStore().addLikeTheme(useTokenStore().token, props.theme.id)
+        status.value = await useThemeStore().checkStatusThemes(useTokenStore().token, props.theme.id)
+    }
+}
+
+async function addRemoveDislikeTheme() {
+    if(status.value == -1) {
+        await useThemeStore().removeLikeDislikeTheme(useTokenStore().token, props.theme.id)
+        status.value = await useThemeStore().checkStatusThemes(useTokenStore().token, props.theme.id)
+    } else {
+        await useThemeStore().addDislikeTheme(useTokenStore().token, props.theme.id)
+        status.value = await useThemeStore().checkStatusThemes(useTokenStore().token, props.theme.id)
+    }
+}
+
+
+onMounted(async () => {
+  status.value = await useThemeStore().checkStatusThemes(useTokenStore().token, props.theme.id)
+})
+</script>
+
+<template>
+  <div class="d-flex justify-space-between align-center">
+    <b>{{ props.theme.name }}</b>
+    <div class="ml-4">
+        <v-btn @click="addRemoveLikeTheme()" v-if="status == 1" density="compact" icon="mdi-thumb-up"></v-btn>
+        <v-btn @click="addRemoveLikeTheme()" v-else density="compact" icon="mdi-thumb-up-outline"></v-btn>
+
+        <v-btn @click="addRemoveDislikeTheme()" v-if="status == -1" density="compact" icon="mdi-thumb-down"></v-btn>
+        <v-btn @click="addRemoveDislikeTheme()" v-else density="compact" icon="mdi-thumb-down-outline"></v-btn> 
+    </div>
+  </div>
+</template>
