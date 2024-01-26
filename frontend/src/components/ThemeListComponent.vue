b72d9772dc88edd439b4674387abbc600128e196
Alexandr Potapov <San_sanih99@mail.ru>
2024-01-26 14:57:16 +0300

    Прописа начальный компонент выпадающего списка тем

--- frontend/src/components/ThemeListComponent.vue
+++ frontend/src/components/ThemeListComponent.vue
@@ -3,6 +3,11 @@ import { useThemeStore } from '@/stores/themes'
 import { useTokenStore } from '@/stores/token'
 import { onMounted } from 'vue'
 
+import ThemeItemComponent from '@/components/ThemeItemComponent.vue' 
+
+// TODO Сделать отображение новых тем после добавления статьи
+// TODO Сделать отображение новых тем после обновления статьи
+// TODO Сделать отображение новых тем после обновления списка статьей
 onMounted(() => {
   useThemeStore().getThemes(useTokenStore().token)
 })
@@ -12,10 +17,15 @@ onMounted(() => {
   <v-expansion-panels class="h-25">
     <v-expansion-panel class="h-25">
       <v-expansion-panel-title> Темы </v-expansion-panel-title>
-      <v-expansion-panel-text v-for="theme in useThemeStore().themes" :key="theme.id">
-        <b >
-            {{ theme.name }}
-        </b>
+      <v-expansion-panel-text>
+        <v-virtual-scroll
+          :height="useThemeStore().themes.length >= 3 ? 100 : useThemeStore().themes.length * 25"
+          :items="useThemeStore().themes"
+        >
+          <template v-slot:default="{ item }">
+            <ThemeItemComponent :theme="item"/>
+          </template>
+        </v-virtual-scroll>
       </v-expansion-panel-text>
     </v-expansion-panel>
   </v-expansion-panels>
