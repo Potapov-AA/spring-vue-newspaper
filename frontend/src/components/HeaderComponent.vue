b5f54ae64f3b7aee6c1786a6afcff7c18b7ca6d0
Alexandr Potapov <San_sanih99@mail.ru>
2024-02-02 15:16:01 +0300

    Исправил ошибку из-за которой при нажатии на кнопку добавления статьи, происходил выход из аккаунта

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -4,24 +4,27 @@ import NewArticleComponent from '@/components/NewArticleComponent.vue'
 </script>
 
 <template>
-  <v-sheet width="100%" :elevation="6" class="header px-6 ">
+  <v-sheet width="100%" :elevation="6" class="header px-6">
     <div class="d-flex align-center justify-space-between">
       <p class="title">The Daily Bugle</p>
 
-      <div @click="useTokenStore().forgetToken()" v-if="useTokenStore().logined" class="d-flex flex-column align-end">
+      <div v-if="useTokenStore().logined" class="d-flex flex-column align-end">
         <div class="user">
           {{ useTokenStore().firstname }}
           {{ useTokenStore().lastname }}
-          <v-btn color="red" icon="mdi-exit-to-app" variant="plain"/>
+          <v-btn
+            @click="useTokenStore().forgetToken()"
+            color="red"
+            icon="mdi-exit-to-app"
+            variant="plain"
+          />
         </div>
 
-        <NewArticleComponent v-if="useTokenStore().role == ROLES.ADMIN"/>
+        <NewArticleComponent v-if="useTokenStore().role == ROLES.ADMIN" />
       </div>
       <div v-else>
         <v-btn class="enter-btn elevation-12" variant="tonal">
-          <router-link :to="{ name: 'login' }" class="link">
-            Войти
-          </router-link>
+          <router-link :to="{ name: 'login' }" class="link"> Войти </router-link>
         </v-btn>
       </div>
     </div>
@@ -34,17 +37,17 @@ import NewArticleComponent from '@/components/NewArticleComponent.vue'
 }
 
 .title {
-  font-family: "SiteTitle";
+  font-family: 'SiteTitle';
   font-size: 100px;
 }
 
 .user {
-  font-family: "UserHeader";
+  font-family: 'UserHeader';
   font-size: 32px;
 }
 
 .enter-btn {
-  font-family: "FontButton";
+  font-family: 'FontButton';
   font-size: 24px;
 }
 </style>
