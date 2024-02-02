a8ef8995cb148988ae2dc3a31eb23d4c396b6056
Alexandr Potapov <San_sanih99@mail.ru>
2024-02-02 13:48:23 +0300

    Изменен шрифт для инфы о пользователе; Настроена кнопка входа; Переделана кнопка выхода;

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -1,32 +1,24 @@
 <script setup>
-import { useTokenStore } from '@/stores/token'
+import { useTokenStore, ROLES } from '@/stores/token'
 import NewArticleComponent from '@/components/NewArticleComponent.vue'
-import ThemeListComponent from '@/components/ThemeListComponent.vue'
 </script>
 
-<!-- TODO Выбрать более подходящий шрифт -->
-<!-- TODO Сделать красивый блок пользователя, укомповать: имя, фамилию, темы, кнопку выхода -->
-<!-- TODO Перенести темы в отдельную боковую панель -->
 <template>
-  <v-sheet width="100%" :elevation="4" class="px-6">
+  <v-sheet width="100%" :elevation="6" class="header px-6 ">
     <div class="d-flex align-center justify-space-between">
       <p class="title">The Daily Bugle</p>
-      <div v-if="useTokenStore().logined" class="d-flex flex-column justify-center">
+
+      <div @click="useTokenStore().forgetToken()" v-if="useTokenStore().logined" class="d-flex flex-column align-end">
         <div class="user">
           {{ useTokenStore().firstname }}
           {{ useTokenStore().lastname }}
+          <v-btn color="red" icon="mdi-exit-to-app" variant="plain"/>
         </div>
-        <NewArticleComponent/>
-        
-        <v-btn @click="useTokenStore().forgetToken()" type="button">
-          Выход
-        </v-btn>
-        <!-- <ThemeListComponent v-if="useTokenStore().logined" class="mb-4"/> -->
 
-        
+        <NewArticleComponent v-if="useTokenStore().role == ROLES.ADMIN"/>
       </div>
       <div v-else>
-        <v-btn class="mx-2">
+        <v-btn class="enter-btn elevation-12" variant="tonal">
           <router-link :to="{ name: 'login' }" class="link">
             Войти
           </router-link>
@@ -37,6 +29,10 @@ import ThemeListComponent from '@/components/ThemeListComponent.vue'
 </template>
 
 <style scoped>
+.header {
+  min-height: 10vh;
+}
+
 .title {
   font-family: "SiteTitle";
   font-size: 100px;
@@ -45,5 +41,10 @@ import ThemeListComponent from '@/components/ThemeListComponent.vue'
 .user {
   font-family: "UserHeader";
   font-size: 32px;
+}
+
+.enter-btn {
+  font-family: "FontButton";
+  font-size: 24px;
 }
 </style>
