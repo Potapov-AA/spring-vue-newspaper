5e8be83621946b580b1ee8d12d6d1909755a5a9f
Alexandr Potapov <San_sanih99@mail.ru>
2024-02-02 21:57:50 +0300

    Поправил оформление

--- frontend/src/components/UpdateArticleComponent.vue
+++ frontend/src/components/UpdateArticleComponent.vue
@@ -21,147 +21,159 @@ const status = ref()
 
 // Функция закрывания окна добавления статьи
 async function closeDialog() {
-    await clearField()    
-    snackbar.value = true
-    showUpdateArticleDialogWindow.value = false
-    await updateListArticles()
+  await clearField()
+  snackbar.value = true
+  showUpdateArticleDialogWindow.value = false
+  await updateListArticles()
 }
 
 // Функция очистки полей
 async function clearField() {
-    title.value = props.article.title
-    themes.value = props.article.themes.join('; ')
-    text.value = props.article.text
-    image.value = await base64ToFile(props.article.image)
-    status.value = -1
+  title.value = props.article.title
+  themes.value = props.article.themes.join('; ')
+  text.value = props.article.text
+  image.value = await base64ToFile(props.article.image)
+  status.value = -1
 }
 
 async function updateArticle() {
-    let themesArray = []
-    if (themes.value != '') {
-        themes.value = themes.value.replaceAll(' ', '')
-        themesArray = themes.value.split(';')
-    }
+  let themesArray = []
+  if (themes.value != '') {
+    themes.value = themes.value.replaceAll(' ', '')
+    themesArray = themes.value.split(';')
+  }
 
-    if (image.value == null) {
-        let result = await useArticleStore().updateArticle(title.value, text.value, null, themesArray, props.article.id, useTokenStore().token)
-        message.value = result.message
-        status.value = result.status
-    } else if (image.value.length == 0) {
-        let result = await useArticleStore().updateArticle(title.value, text.value, null, themesArray, props.article.id, useTokenStore().token)
-        message.value = result.message
-        status.value = result.status
-    }
-     else {
-        var reader = new FileReader()
-        reader.readAsDataURL(image.value[0])
+  if (image.value == null) {
+    let result = await useArticleStore().updateArticle(
+      title.value,
+      text.value,
+      null,
+      themesArray,
+      props.article.id,
+      useTokenStore().token
+    )
+    message.value = result.message
+    status.value = result.status
+  } else if (image.value.length == 0) {
+    let result = await useArticleStore().updateArticle(
+      title.value,
+      text.value,
+      null,
+      themesArray,
+      props.article.id,
+      useTokenStore().token
+    )
+    message.value = result.message
+    status.value = result.status
+  } else {
+    var reader = new FileReader()
+    reader.readAsDataURL(image.value[0])
 
-        reader.addEventListener('load', async (e) => {
-            let imageToBase64 = e.target.result
+    reader.addEventListener('load', async (e) => {
+      let imageToBase64 = e.target.result
 
-            let result = await useArticleStore().updateArticle(title.value, text.value, imageToBase64, themesArray, props.article.id, useTokenStore().token)
+      let result = await useArticleStore().updateArticle(
+        title.value,
+        text.value,
+        imageToBase64,
+        themesArray,
+        props.article.id,
+        useTokenStore().token
+      )
 
-            message.value = result.message
-            status.value = result.status
+      message.value = result.message
+      status.value = result.status
 
-            if(status.value == 200) {
-                await base64ToImage(imageToBase64, props.article.id)
-                await closeDialog()
-            }
-        })
-    }
+      if (status.value == 200) {
+        await base64ToImage(imageToBase64, props.article.id)
+        await closeDialog()
+      }
+    })
+  }
 }
 
 onMounted(async () => {
-    if(props.article.image != null) {
-        image.value = await base64ToFile(props.article.image)
-    }
+  if (props.article.image != null) {
+    image.value = await base64ToFile(props.article.image)
+  }
 })
 </script>
 
 <template>
-    <div>
-        <v-btn icon v-if="useTokenStore().role == ROLES.ADMIN" variant="text">
-            <v-icon @click="() => {
-                showUpdateArticleDialogWindow = true
-            }">{{ 'mdi-pen' }}</v-icon>
+  <div>
+    <v-btn
+      icon="mdi-pen"
+      v-if="useTokenStore().role == ROLES.ADMIN"
+      variant="text"
+      @click="
+        () => {
+          showUpdateArticleDialogWindow = true
+        }
+      "
+    />
+
+    <v-dialog
+      v-model="showUpdateArticleDialogWindow"
+      fullscreen
+      :scrim="false"
+      transition="dialog-bottom-transition"
+    >
+      <v-card>
+        <v-card-title class="d-flex justify-center mt-5">
+          <span class="form-title">Редактирование статьи {{ props.article.title }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-text-field
+              v-model="title"
+              label="Название"
+              placeholder="Человек-Паук снова актакует!"
+            />
+            <v-text-field v-model="themes" label="Темы" placeholder="Паук; DC; Marvel" />
+            <v-textarea v-model="text" label="Текст" />
+            <v-file-input v-model="image" accept="image/*" label="Изображение" />
+            <p v-if="status != 200 && status != -1" class="form-text">{{ message }}</p>
+          </v-container>
+        </v-card-text>
+        <v-card-actions class="d-flex justify-end mb-15 mr-15">
+          <v-btn
+            color="red-darken-1"
+            variant="text"
+            @click="
+              () => {
+                clearField()
+                showUpdateArticleDialogWindow = false
+              }
+            "
+            class="form-btn"
+          >
+            Закрыть
+          </v-btn>
+          <v-btn
+            color="blue-darken-1"
+            variant="text"
+            id="file_input"
+            @click="
+              () => {
+                updateArticle()
+              }
+            "
+            class="form-btn"
+          >
+            Обновить
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-snackbar v-model="snackbar" :timeout="2000">
+      <p class="form-text mt-2">{{ message }}</p>
+
+      <template v-slot:actions>
+        <v-btn color="blue" variant="text" @click="snackbar = false" class="form-btn">
+          Закрыть
         </v-btn>
-
-        <v-dialog 
-            v-model="showUpdateArticleDialogWindow" 
-            fullscreen
-            :scrim="false"
-            transition="dialog-bottom-transition"
-        >
-            
-        <v-card>
-            <v-card-title class="d-flex justify-center mt-5">
-                <span class="form-title">Редактирование статьи {{props.article.title}}</span>
-            </v-card-title>
-            <v-card-text>
-                <v-container>
-                    <v-text-field
-                        v-model = title
-                        label="Название"
-                        placeholder="Человек-Паук снова актакует!"
-                    />
-                    <v-text-field
-                        v-model = themes
-                        label="Темы"
-                        placeholder="Паук; DC; Marvel"
-                    />
-                    <v-textarea
-                        v-model = text
-                        label="Текст"    
-                    />
-                    <v-file-input
-                        v-model = image
-                        accept="image/*" 
-                        label="Изображение" 
-                    />
-                    <p v-if="status != 200 && status != -1" class="form-text">{{ message }}</p>
-                </v-container>
-            </v-card-text>
-            <v-card-actions  class="d-flex justify-end mb-15 mr-15">
-                <v-btn
-                    color="red-darken-1"
-                    variant="text"
-                    @click="() => {
-                        clearField()
-                        showUpdateArticleDialogWindow = false
-                    }"
-                    class="form-btn"
-                >
-                    Закрыть
-                </v-btn>
-                <v-btn
-                    color="blue-darken-1"
-                    variant="text"
-                    id="file_input"
-                    @click="() => {
-                        updateArticle()
-                    }"
-                    class="form-btn"
-                >
-                    Обновить
-                </v-btn>
-            </v-card-actions>
-        </v-card>
-        </v-dialog>
-
-        <v-snackbar v-model="snackbar" :timeout="2000">
-            <p class="form-text mt-2">{{ message }}</p>
-            
-            <template v-slot:actions>
-                <v-btn 
-                    color="blue" 
-                    variant="text" 
-                    @click="snackbar = false"
-                    class="form-btn"
-                > 
-                    Закрыть 
-                </v-btn>
-            </template>
-        </v-snackbar>
+      </template>
+    </v-snackbar>
   </div>
 </template>
