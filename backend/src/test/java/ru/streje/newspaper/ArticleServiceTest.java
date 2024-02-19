94a92cd44629015a97a3b896b5e11dc05117166b
Alexandr Potapov <San_sanih99@mail.ru>
2024-02-19 19:22:19 +0300

    Начал покрывать тестами

---
+++ backend/src/test/java/ru/streje/newspaper/ArticleServiceTest.java
@@ -0,0 +1,15 @@
+package ru.streje.newspaper;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import ru.streje.newspaper.repositories.ArticleRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ArticleServiceTest {
+		
+}
