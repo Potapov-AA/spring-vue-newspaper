863f50388885ea0cd8e019ef3be9a06282830605
Alexandr Potapov <San_sanih99@mail.ru>
2024-02-26 14:50:31 +0300

    Изменено Iterable на Collection

--- backend/src/main/java/ru/streje/newspaper/repositories/ArticleRepository.java
+++ backend/src/main/java/ru/streje/newspaper/repositories/ArticleRepository.java
@@ -1,9 +1,11 @@
 package ru.streje.newspaper.repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ru.streje.newspaper.models.Article;
 
 public interface ArticleRepository extends CrudRepository<Article, Integer> {
-	Iterable<Article> findByOrderByIdDesc();
+	Collection<Article> findByOrderByIdDesc();
 }
