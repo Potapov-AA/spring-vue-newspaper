dea43fdb2c81febf519fd48b30cc65a48de8ff66
Alexandr Potapov <San_sanih99@mail.ru>
2024-03-11 13:34:08 +0300

    Добавлен интерфейс RoleService и его реализация

--- backend/src/main/java/ru/streje/newspaper/services/RoleService.java
+++ backend/src/main/java/ru/streje/newspaper/services/RoleService.java
@@ -1,22 +1,8 @@
 package ru.streje.newspaper.services;
 
-import org.springframework.stereotype.Service;
-
-import lombok.RequiredArgsConstructor;
 import ru.streje.newspaper.models.Role;
-import ru.streje.newspaper.repositories.RoleRepository;
 
-@Service
-@RequiredArgsConstructor
-public class RoleService {
-	private final RoleRepository roleRepository;
-
-	/**
-	 * Получение экземпляр класса Role с названием USER
-	 * 
-	 * @return экземпляр класса Role
-	 */
-	public Role getUserRole() {
-		return roleRepository.findByName("ROLE_USER").get();
-	}
+public interface RoleService {
+	
+	public Role getUserRole();
 }
