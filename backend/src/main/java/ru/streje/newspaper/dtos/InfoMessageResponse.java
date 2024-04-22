4c691386ba0bb22625aacada6aec25c3dd393602
Alexandr Potapov <San_sanih99@mail.ru>
2024-04-22 17:35:20 +0300

    Переименовал

--- backend/src/main/java/ru/streje/newspaper/dtos/InfoMessageResponse.java
+++ backend/src/main/java/ru/streje/newspaper/dtos/InfoMessageResponse.java
@@ -5,12 +5,12 @@ import java.util.Date;
 import lombok.Data;
 
 @Data
-public class InfoMessage {
+public class InfoMessageResponse {
 	private int status;
 	private String message;
 	private Date timestamp;
 	
-	public InfoMessage(int status, String message) {
+	public InfoMessageResponse(int status, String message) {
 		this.status = status;
 		this.message = message;
 		this.timestamp = new Date();
